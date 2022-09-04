d41a4b8ad19fd99849c32381e604130f6fd46159
INBUM02 <kib0202@naver.com>
2022-09-04 21:25:11 +0900

    FEAT: 기본 반응형 설정 및 글꼴 설정

--- src/components/layouts/AppBar.tsx
+++ src/components/layouts/AppBar.tsx
@@ -17,14 +17,31 @@ function AppBar() {
           className="logo"
           width="192px"
           height="28px"
+          layout="fixed"
         />
       </Logo>
       <Search>
-        <input onChange={inputText} value={text} placeholder="search" />
-        <button>
-          <Image alt="search" src="/assets/search.svg" width="25" height="25" />
-        </button>
+        <div className="when-wide">
+          <input onChange={inputText} value={text} placeholder="search" />
+          <button>
+            <Image
+              alt="search"
+              src="/assets/search-white.svg"
+              width="25"
+              height="25"
+            />
+          </button>
+        </div>
+        <div className="when-narrow">
+          <Image
+            alt="search"
+            src="/assets/search-blue.svg"
+            width="25"
+            height="25"
+          />
+        </div>
       </Search>
+      <Mypage>Login</Mypage>
     </Wrap>
   );
 }
@@ -33,38 +50,62 @@ export default AppBar;
 
 const Wrap = styled.div`
   background-color: white;
-  height: 135px;
+  height: 115px;
   display: flex;
 `;
 
+const Search = styled.div`
+  flex: 1;
+  .when-wide {
+    width: 100%;
+    display: flex;
+    margin-top: 32px;
+    height: 48px;
+    background-color: #f3f3f3;
+    border-radius: 8px;
+    input {
+      padding-left: 38px;
+      border: none;
+      background-color: #f3f3f3;
+      flex: 1;
+      font-size: 18px;
+      font-weight: 400;
+      color: #c5c5c5;
+      border-radius: 8px;
+    }
+    button {
+      background-color: blue;
+      width: 79px;
+      height: 48px;
+      border-radius: 8px;
+      border: none;
+    }
+  }
+  .when-narrow {
+    display: none;
+  }
+  @media screen and (max-width: 650px) {
+    .when-wide {
+      display: none;
+    }
+    .when-narrow {
+      display: block;
+      margin-top: 45px;
+      text-align: right;
+      margin-right: 10px;
+    }
+  }
+`;
+
 const Logo = styled.div`
-  margin-top: 62px;
+  margin-top: 42px;
   margin-right: 30px;
   margin-left: 30px;
 `;
 
-const Search = styled.div`
-  display: flex;
-  margin-top: 52px;
-  height: 48px;
-  background-color: #f3f3f3;
-  width: 500px;
-  border-radius: 8px;
-  input {
-    padding-left: 38px;
-    border: none;
-    background-color: #f3f3f3;
-    flex: 1;
-    font-size: 18px;
-    font-weight: 400;
-    color: #c5c5c5;
-    border-radius: 8px;
-  }
-  button {
-    background-color: blue;
-    width: 79px;
-    height: 48px;
-    border-radius: 8px;
-    border: none;
-  }
+const Mypage = styled.div`
+  width: 70px;
+  color: blue;
+  line-height: 115px;
+  text-align: center;
 `;
