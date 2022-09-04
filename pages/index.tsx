b2c6d3c77c848141d06c90c4d5d30a93df928022
INBUM02 <kib0202@naver.com>
2022-09-05 04:28:54 +0900

    FEAT: 배포환경 확인후 다시 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,6 @@ import { useSession, signIn, signOut } from "next-auth/react";
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
   console.log(session, status);
-  console.log(
-    process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID,
-    process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET,
-    typeof process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET
-  );
   if (session) {
     return (
       <>
