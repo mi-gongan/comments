e3391782bff2cb53fd775ba2b6e99d183e98cc08
INBUM02 <kib0202@naver.com>
2022-09-25 19:03:49 +0900

    F::gtm down

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,15 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W83RM4V');`,
+            }}
+          />
+          <script
             defer
             src="https://developers.kakao.com/sdk/js/kakao.min.js"
           ></script>
@@ -16,6 +25,12 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W83RM4V"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
         </body>
       </Html>
     );
