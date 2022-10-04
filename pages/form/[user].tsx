237f84fc0fde7ed732b80bce1f851cf795339841
INBUM02 <kib0202@naver.com>
2022-10-05 01:13:52 +0900

    FIXED:image test

--- pages/form/[user].tsx
+++ pages/form/[user].tsx
@@ -22,7 +22,10 @@ function Form() {
         <meta property="og:title" content={`코맨션 적으러 가기`} />
         <meta property="og:description" content={getMessage(relation)} />
         <meta property="og:url" content={linkFormat} />
-        <meta property="og:image" content={getImg(relation)} />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/comments-efbc8.appspot.com/o/share_img_peer.svg?alt=media&token=793b2570-067d-4d5f-9963-6d724ea6eefc"
+        />
       </Head>
       <GrobalStyled />
       <Commention></Commention>
