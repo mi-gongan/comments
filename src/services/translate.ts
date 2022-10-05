5c27c07a67c7d8f8001b9f175a30263ca4c72e7a
INBUM02 <kib0202@naver.com>
2022-10-06 01:57:33 +0900

    F:image

--- src/services/translate.ts
+++ src/services/translate.ts
@@ -16,15 +16,15 @@ export const matchType = (type: string) => {
 export const getImg = (relation: string) => {
   switch (relation) {
     case "동료":
-      return "/assets/share_img_peer.svg";
+      return "/peer.svg";
     case "가족":
-      return "/assets/share_img_family.svg";
+      return "/family.svg";
     case "친구":
-      return "/assets/share_img_friend.svg";
+      return "/friend.svg";
     case "연인":
-      return "/assets/share_img_couple.svg";
+      return "/couple.svg";
     default:
-      return "/assets/share_img_etc.svg";
+      return "/etc.svg";
   }
 };
 
